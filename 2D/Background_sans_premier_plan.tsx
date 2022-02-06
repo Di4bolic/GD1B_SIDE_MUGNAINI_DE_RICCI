e13e2c4dcb5aba10d1634b03e733122c6cd0e8c0
Ethan Mugnaïni de Ricci <93705253+Di4bolic@users.noreply.github.com>
2022-02-06 16:00:29 +0100

    Add files via upload

---
+++ 2D/Background_sans_premier_plan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Background_sans_premier_plan" tilewidth="1920" tileheight="3240" tilecount="1" columns="1">
+ <image source="../PROG/assets/Background_sans_premier_plan.png" width="1920" height="3240"/>
+</tileset>
